cc0dfa669080dae0f38ca12be156874a0b182e37
dcuenot <11711525+dcuenot@users.noreply.github.com>
2020-04-30 15:26:54 +0200

    feat(display-condition): fix env var issue

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# cli-sonar-quality-gate-result
+# CLI to get Qualtiy Gate of SonarQube
+
+![Continuous integration](https://github.com/dcuenot/sonar-qg-in-cli/workflows/Continuous%20integration/badge.svg)
+![Security audit](https://github.com/dcuenot/sonar-qg-in-cli/workflows/Security%20audit/badge.svg)
+[![codecov](https://codecov.io/gh/dcuenot/sonar-qg-in-cli/branch/master/graph/badge.svg)](https://codecov.io/gh/dcuenot/sonar-qg-in-cli)
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,5 @@
 #[macro_use]
 extern crate serde_derive;
-#[macro_use]
-extern crate dotenv_codegen;
 
 use anyhow::Error;
 use domain::sonar::QualityStatus;
@@ -12,7 +10,7 @@ pub mod domain;
 pub mod infra;
 
 pub fn yolo(analysis_id: &str) -> Result<QualityStatus, Error> {
-    let sonar_client = SonarClient::new(dotenv!("SONAR_URL"), dotenv!("SONAR_TOKEN"));
+    let sonar_client = SonarClient::new(env!("SONAR_URL", "plop"), env!("SONAR_TOKEN", "yolo"));
 
     let mut rt = Runtime::new().expect("tokio runtime can be initialized");
     rt.block_on(async move { sonar_client.quality_gate_status(analysis_id).await })
